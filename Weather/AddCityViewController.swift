754036c2d2ad14627d3492c513cfb86eb5eba306
KAK <kak@gmail.com>
2017-02-25 23:33:13 -0500

    Add city VC modified

--- Weather/AddCityViewController.swift
+++ Weather/AddCityViewController.swift
@@ -8,36 +8,16 @@
 
 import UIKit
 
-class AddCityViewController: UIViewController, UITableViewDataSource {
+class AddCityViewController: UIViewController {
     
     @IBOutlet weak var citiesTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        citiesTableView.dataSource = self
+        self.title = "Add City"
     }
     
-    @IBAction func onDonePressed(_ sender: UIBarButtonItem) {
-        self.dismiss(animated: true, completion: nil)
-    }
     
-    //MARK: TableView Datasource Methods
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return WeatherData.sharedData.listOfCities.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cityNameCell", for: indexPath)
-        
-        let city = WeatherData.sharedData.listOfCities[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = city.name
-        cell.detailTextLabel?.text = city.country
-        
-        return cell
-    }
+    //MARK: Methods
+
 }

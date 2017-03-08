cbb22f42f67359b4008787a21aa1d16d989b37dd
KAK <kak@gmail.com>
2017-03-08 14:36:24 -0500

    Locations class created

---
+++ Locations.swift
@@ -0,0 +1,17 @@
+//
+//  Locations.swift
+//  Weather
+//
+//  Created by Vasanth Kodeboyina on 3/7/17.
+//  Copyright © 2017 Anish Kodeboyina. All rights reserved.
+//
+
+import Foundation
+
+class Locations {
+    var location: String
+    
+    init(location:String) {
+        self.location = location
+    }
+}

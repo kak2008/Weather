7a9a463bd21c21a264ad55e15b1a3df042783f39
KAK <kak@gmail.com>
2017-03-10 01:01:27 -0500

    API call initiated

--- Weather/CitiesTableViewController.swift
+++ Weather/CitiesTableViewController.swift
@@ -12,8 +12,8 @@ class CitiesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        getWeatherDataCalling()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +21,17 @@ class CitiesTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func getWeatherDataCalling() -> Void
+    {
+        let apiObj = ApiClient()
+        apiObj.getWeatherData("Michigan", failure: { (errorMessage) in
+        }) {
+            DispatchQueue.main.async(execute: {
+                self.tableView.reloadData()
+            })
+
+        }
+    }
 
     // MARK: - Table View Methods
 
@@ -29,16 +40,41 @@ class CitiesTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let cities = WeatherData.sharedData.listOfCities
+        return cities.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath) as! CityTableViewCell
-        cell.cityTimeLabel.text = "07:12 PM"
-        cell.cityNamelabel.text = "Hyderabad"
-        cell.cityWeatherDescriptionLabel.text = "Clear Sky"
-        cell.cityWeatherTempLabel.text = NSString(format: "45%@", "\u{00b0}") as String
         
+        var cities = WeatherData.sharedData.listOfCities
+        var weatherInfo = WeatherData.sharedData.weatherList
+        
+        let cityObj = cities[indexPath.row]
+        let weatherObj = weatherInfo[indexPath.row]
+        
+        cell.cityTimeLabel.text = "07:12 PM"
+        cell.cityNamelabel.text = "\(cityObj.country)"
+        cell.cityWeatherDescriptionLabel.text = "\(weatherObj.description)"
+        
+        let convertedTemp = convertTempUnits(temp: weatherObj.temperature, conversionType: "Celsius")
+        let tempValue = convertDecimalToString(temp: convertedTemp)
+        cell.cityWeatherTempLabel.text = NSString(format: "\(tempValue)%@" as NSString, "\u{00b0}") as String
+       
         return cell
+    }
+    
+    func convertTempUnits(temp: Double, conversionType: String) -> Double {
+        switch conversionType {
+        case "Celsius": return (temp - 273.15)
+        case "Fahrenheit": return ((temp - 273.15) * 9/5 ) + 32
+        default: return temp
+        }
+    }
+    
+    func convertDecimalToString(temp: Double) -> String {
+        let tempValue = "\(temp)"
+        let splitValue = tempValue.characters.split(separator: ".")
+        return String(splitValue.first!)
     }
 }

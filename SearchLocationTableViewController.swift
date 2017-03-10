56452be3e9b52720dd1cf2eeefb71417622aef24
KAK <kak@gmail.com>
2017-03-10 01:01:44 -0500

    placeholder text has been updated

--- SearchLocationTableViewController.swift
+++ SearchLocationTableViewController.swift
@@ -14,14 +14,16 @@ class SearchLocationTableViewController: UITableViewController,UISearchResultsUp
     var currentLocations = [Locations]()
     var filteredLocations = [Locations]()
     let searchController = UISearchController(searchResultsController: nil)
+    var selectedCity: String!
     
     // MARK: - IBOutlets
     @IBOutlet var searchTableView: UITableView!
+    @IBOutlet weak var locationSearchBar: UISearchBar!
     
     // MARK: - View Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Add City"
         currentLocations =
             [Locations(location: "Hyderabad"),
              Locations(location: "Mumbai"),
@@ -35,10 +37,12 @@ class SearchLocationTableViewController: UITableViewController,UISearchResultsUp
              Locations(location: "Simla"),
              Locations(location: "Kolkata")]
         
+        searchController.searchBar.placeholder = "Enter City, Zip code, or airport location"
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
         definesPresentationContext = true
         tableView.tableHeaderView = searchController.searchBar
+ 
     }
 
     override func didReceiveMemoryWarning() {
@@ -68,7 +72,9 @@ class SearchLocationTableViewController: UITableViewController,UISearchResultsUp
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(filteredLocations[indexPath.row].location)
+        selectedCity = filteredLocations[indexPath.row].location
+        print(selectedCity)
+        _ = navigationController?.popViewController(animated: true)
     }
  
     // MARK: - Search Methods
@@ -88,50 +94,5 @@ class SearchLocationTableViewController: UITableViewController,UISearchResultsUp
         }
         tableView.reloadData()
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

5f70a2d30606082a06466348b66da6ffbf6153f2
KAK <kak@gmail.com>
2017-03-11 13:10:11 -0500

    Swift constants added.

---
+++ Weather/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Vasanth Kodeboyina on 3/11/17.
+//  Copyright © 2017 Anish Kodeboyina. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let someConstant = "TEST"
+}
+
+struct UISymbols {
+    static let tempDegreeSymbol = "\u{00b0}"
+}
+
+struct CellIdentifiers {
+    static let cityTableViewCell = "cityCell"
+}
+
+struct TempUnits {
+    static let Celsius = "Celsius"
+    static let Fahrenheit = "Fahrenheit"
+}

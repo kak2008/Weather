09b2390bb09b348dc7517d4b5c935b0ea91dbe47
KAK <kak@gmail.com>
2017-03-17 01:00:17 -0400

    Api call for city data with zipcode

--- Weather/ApiClient.swift
+++ Weather/ApiClient.swift
@@ -18,19 +18,15 @@ class ApiClient: NSObject
         
         // URL Request Session
         let conf = URLSessionConfiguration.default
-        
         let session = URLSession(configuration: conf)
-
         let task = session.dataTask(with: url!, completionHandler: { (data, response, error) in
            
             // error handling
-            if error != nil
-            {
+            if error != nil {
                 print(error as Any)
                 // ToDo:- create Alert with error
                 return
             }
-            
             
             do {
                 // Data conversion: NSData to Json
@@ -39,14 +35,39 @@ class ApiClient: NSObject
                 WeatherData.sharedData.updateCityData(receivedData)
                 success()
             }
-            catch let error1 as NSError
-            {
+            catch let error1 as NSError {
                 //     print("Unable to parse the response data!")
                 // ToDo:- create Alert with error
             
                 print(error1)
             }
         }) 
+        task.resume()
+    }
+
+    func getCityWithZipcode(zipcode: String, failure: (_ errorMessage: String) -> Void, success: @escaping() -> Void) {
+        // URL Request creation
+        let urlString = "http://maps.googleapis.com/maps/api/geocode/json?address=\(zipcode)&sensor=true"
+        let url = URL(string: urlString)
+        
+        // URL Request Session
+        let conf = URLSessionConfiguration.default
+        let session = URLSession(configuration: conf)
+        let task = session.dataTask(with: url!, completionHandler: { (data, response, error) in
+            // error handling
+            if error != nil {
+                print(error as Any)
+                return
+            }
+            do {
+                let receivedData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! NSDictionary
+                WeatherData.sharedData.updateZipcodeCities(receivedData)
+                success()
+            }
+            catch let error as NSError {
+                print(error)
+            }
+        })
         task.resume()
     }
 }
